bce67069e81880c43b53a33cdfafcd82963e9ce3
Francisco Valdiney Delmiro Silva <valdineysilva112@gmail.com>
2025-10-12 20:41:35 -0300

    Melhorado os textos das listas, adicionado a descrição da imagem no modo light e adicionado o IG do instagram no footer

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ function toggleMode() {
   // pegar a tag imagem
   const image = document.querySelector("#profile img")
 
+  let modeLight = html.classList.contains("light")
   // substituir a imagem
-      // se tiver light mode adicionar a imagem
-      //se tiver sem light mode manter a imagem normal
-  html.classList.contains("light")
+  // se tiver light mode adicionar a imagem
+  //se tiver sem light mode manter a imagem normal
+  modeLight
     ? image.setAttribute("src", "./assets/mobile/avatar-light.png")
     : image.setAttribute("src", "./assets/mobile/avatar.png")
+
+  modeLight
+    ? image.setAttribute("alt","Foto de Valdiney sorrindo usando óculos e camisa preta barba e fundo restaurante na praia")
+    : image.setAttribute("alt","Selfie de Valdiney neutro usando óculos e camisa branca com o nascer do sol no fundo")
 }
